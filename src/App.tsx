1ea5752af60e17031cae02028cb03b5ba372e6dd
sowon_m1 <bsw2377@gmail.com>
2024-03-01 17:05:10 +0900

    title 변경

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 const App = () => {
   return (
     <div className="mx-auto my-5 w-3/4 text-center">
-      <h2 className="my-3 text-3xl font-bold">오늘 할 일</h2>
+      <h2 className="my-3 text-3xl font-bold">React Todo List</h2>
       <List />
     </div>
   );
